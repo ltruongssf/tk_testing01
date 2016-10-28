1d05cbb7f44a1f7550f13584b262aa7219461a44
Lisa Truong <truongglisa@gmail.com>
2016-10-28 22:35:53 +0000

    TK Test 3

--- workspace/TKTest/www/js/controllers/questionsCtrl.js
+++ workspace/TKTest/www/js/controllers/questionsCtrl.js
@@ -1,7 +1,9 @@
+/*global angular */
 angular.module('starter.controllers')
-.controller('QuestionsCtrl',['$scope', '$stateParams', 'testInfo', 'TKAnswersService', '$state', '$ionicHistory',
-function($scope, $stateParams, testInfo, TKAnswersService, $state, $ionicHistory) {
+.controller('QuestionsCtrl',['$scope', '$stateParams', 'testInfo', 'TKAnswersService', '$state', '$ionicHistory', 'TKResultsButtonService', 
+function($scope, $stateParams, testInfo, TKAnswersService, $state, $ionicHistory, TKResultsButtonService) {
     $scope.qNumber = $stateParams.questionID;
+    $scope.shouldShowButton = TKResultsButtonService.getShouldShowMenuButton();
     testInfo.forEach(function(infoDict) {
      if(infoDict.Answer_ID === "A")
           $scope.questionA = infoDict;
@@ -19,20 +21,26 @@ function($scope, $stateParams, testInfo, TKAnswersService, $state, $ionicHistory
           $state.go('question', {questionID: nextqNumber});
         }
 };
+$scope.ptorQuestionGoA = 'ptor-question-go-a' + $stateParams.questionID;
+$scope.ptorQuestionGoB = 'ptor-question-go-b' + $stateParams.questionID;
+$scope.ptorQuestionTextA = 'ptor-question-text-a' + $stateParams.questionID;
+$scope.ptorQuestionTextB = 'ptor-question-text-b' + $stateParams.questionID;
+
 $scope.goBack = function() {
       if($scope.qNumber >1)
         TKAnswersService.eraseLastAnswer();
       $ionicHistory.goBack();
  };
  function performRequest() {
-    var answersDict = angular.copy(TKAnswersService.getAnswers());
-    var date = new Date();
-    answersDict["createDate"] = date.toUTCString();
-    TKAnswersService.saveTest(answersDict);
-    $ionicHistory.nextViewOptions({
+     var answersDict = angular.copy(TKAnswersService.getAnswers());
+     var date = new Date();
+     answersDict["createDate"] = date.toUTCString();
+     TKAnswersService.saveTest(answersDict);
+     TKResultsButtonService.setShouldShowMenuButton(true);
+     $ionicHistory.nextViewOptions({
          historyRoot: true
-    });
-    $state.go('lobby');
-}
-});
-}]); 
+     });
+     $state.go('results');
+ }
+ });
+ }]);
